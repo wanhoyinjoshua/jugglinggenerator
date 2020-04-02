3594e10804dfb19ec6c952a711d9c1d7074c1930
joshua <wanhoyinsjoahu@yahoo.com>
2020-04-02 13:56:00 +0800

    hihi

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-var list = ["sitdown","one leg","look at different backgrounds","juggle with socks", "height of the toss "]
+var list = ["sitdown","one leg","look at different backgrounds","juggle with socks", "height of the toss", "juggle with one eye close"]
 var ran = Math.random()*list.length;
-console.log(ran)
+console.log(ran); 
 var gen = Math.floor(ran);
 console.log(list[gen])
 var hi = document.getElementById("changeee")
 console.log(hi)
 
 var data=[];
+var numdata=[];
 
 var datanum = document.forms[0].elements[0];
+
+var changebutt = document.getElementById("shuufle")
+
+
+hi.innerHTML="Type in 0 to start" ;
+datanum.value = ""
 
 
 
@@ -17,17 +24,39 @@ var datanum = document.forms[0].elements[0];
 function change (){
     console.log(datanum.value)
 
-    if (datanum.value != null){
+    changebutt.disabled = true;
+    setTimeout(function(){ changebutt.disabled =false; }, 2000);
 
-        var ran = Math.random()*list.length;
+    if (isNaN(datanum.value)){
 
-        var gen = Math.floor(ran);
-        hi.innerHTML=list[gen];
+        
+        hi.innerHTML="Give me a number tim , no characters "
+
+    }
+
+    else if (datanum.value==""){
+        hi.innerHTML="Must input sth tim"
 
     }
 
     else{
-        hi.innerHTML="error please type again"
+
+        
+        console.log(data)
+        console.log(numdata)
+      
+
+        data.push(` ${hi.innerText}:${datanum.value}`)
+        numdata.push(datanum.value)
+
+        var ran = Math.random()*list.length;
+
+        var gen = Math.floor(ran);
+        
+
+        
+        hi.innerHTML=list[gen];
+        datanum.value = ""
     }
     
    
@@ -37,8 +66,61 @@ function change (){
 
 function generate(){
 
+    numdata.splice(0,1)
+    
+    console.log(numdata)
+    
+    
+    function getAvg(numdata) {
+
+        var  total = 0;
+
+        for (let i =0; i<numdata.length; i++){
+
+            total += parseInt(numdata[i], 10);
+
+            
+        }
+        return total;
+
+      }
+
+
+    let total = getAvg(numdata);
+    let average = total /  numdata.length;
+    
+    hi.innerHTML=`
+
+    Good Job Tim !!!!!!<br><br>
+    Your totaal attmept in this session is <strong>${numdata.length}</strong> <br><br>
+    Your total no of catches in this session is <strong> ${total}(${total/6} cycles) </strong><br><br>
+    Your avergae no of catches in this session is <strong>${average}(${average/6} cycles)</strong>`
+
+
+    data.splice(0,1) ;
+
+   let shuufle = document.getElementById("shuufle")
+
+   shuufle.parentNode.removeChild(shuufle)
+   let dataa = document.getElementById("genertae")
+
+   dataa.parentNode.removeChild(dataa)
+   let  textinput = document.getElementById("textinput")
+
+   textinput.parentNode.removeChild( textinput)
+  
+
+  
+
+for (let i =0 ; i <data.length;i++){
+    let p = document.createElement("p");
+    p.innerHTML= `${data[i]}`
+    document.body.append(p);
+    }
 
 }
+
+
 
 
 
